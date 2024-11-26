185d7966ba7963501db1f25ea099d77662883534
lemofack <lemofack@3il.fr>
2024-11-26 16:37:03 +0100

    Exercice modifie

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,33 @@
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+    /**
+        * fonction pour afficher la moyenne
+    */
+    
+    public void afficherNotesEtMoyenne(String nomEtudiant,int[] notes) {
+        
         System.out.println("Notes de " + nomEtudiant + ":");
-        for (int note : notes) {
-            System.out.println("- " + note);
-        }
-
-        // Calcul de la somme des notes
-        int somme = 0;
-        for (int note : notes) {
-            somme += note;
-        }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
+        afficherNote();
+        double moyenne = calculsom()/ notes.length;
         System.out.println("Moyenne : " + moyenne);
     }
+    /**
+     * fonction pour afficher  et calculer la note 
+    */
+    public void  calculsom (int[] notes){
+        double  somme =0.0;
+        for(int note : notes){
+            somme +=note;
+        }
+        System.out.println(somme);
+    }
+    /**
+     * afficher les notes 
+     */
+    public void afficherNote(int[] notes){
+        for(int n : notes){
+            System.out.printf("%-5d",n);
+        }
+    }
+    
 }
