c4e97edf5334a2726ae5c79918d688cf696e087b
lemofack <lemofack@3il.fr>
2024-11-26 16:12:23 +0100

    Exercice 1 traite

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,23 +1,62 @@
 public class FactureManager {
-
-    // Calcul le total d'une facture
-    public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
-        double total = quantite * prixUnitaire;
-
-        // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
-        }
-
-        // Reduction sur le total
-        if (total > 1000) {
-            total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
-        }
-
-        return total;
+    /**
+     * declarer les variables globalement et initialiser 
+     */
+    private String typeProduit;
+    private int quantite ;
+    private  double prixUnitaire;
+    private double total ;
+    /**
+     * declaration du constructeur pour initialiser mes valeurs
+     */
+    public FactureManager(String typeProduit, int quantite,double prixUnitaire, double total ){
+        this.typeProduit=typeProduit;
+        this.quantite=quantite;
+        this.prixUnitaire=prixUnitaire;
+        this.total=total;
     }
+    /**
+     * methodes pour la facture et sa rduction
+     */
+    public factureReduction(){
+        
+        String Produit =reduction();
+        total = calculerFacture();
+        // Réduction supplémentaire de 5% pour les gros montants
+        if (total > 1000) {
+            total -= total * 0.05; 
+        }
+            return total ;
+        }
+    }
+
+    /**
+     * fonction pour calculer une facture;
+     */
+    public double calculerFacture() {
+
+        return quantite * prixUnitaire;
+    }
+    /**
+     * reduction sur une facture
+     */
+    public reduction(){
+        switch (typeProduit){
+            case "Alimentation":
+                total-=total *0.05;
+            break ;
+            case "Electronique":
+                total-=total * 0.1;
+            break;
+            case "Luxe" : 
+                total -=total * 0.15;
+            break;
+            default :
+            System.out.println("pas de reduction a effectuer");
+            break;
+        }
+    }
+        
+        
+    
 }
